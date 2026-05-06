1a4077a3292569fb6376af120b6c4aeb6b632a40
TimmyTimmych <159017794+TimmyTimmych@users.noreply.github.com>
2026-05-06 23:48:18 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,30 @@
 
+const hydrants = [
+  {
+    name: "Hydrant 1",
+    lat: 50.4501,
+    lng: 30.5234,
+    type: "hydrant"
+  },
+  {
+    name: "Hydrant 2",
+    lat: 50.4520,
+    lng: 30.5200,
+    type: "hydrant"
+  }
+];
+
+const water = [
+  {
+    name: "Water 1",
+    lat: 50.4550,
+    lng: 30.5300,
+    type: "water"
+  },
+  {
+    name: "Water 2",
+    lat: 50.4570,
+    lng: 30.5350,
+    type: "water"
+  }
+];
